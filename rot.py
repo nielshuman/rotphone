c86c9bde28b982a69c6e57f681ae0a13340a446b
Niels <nielsz@pm.me>
2025-05-05 12:47:01 +0200

    BOEM

--- rot.py
+++ rot.py
@@ -1,3 +1,17 @@
 from gpiozero import Button
 
-print('yo!')
+t1 = Button(21, bounce_time=0.01)
+t2 = Button(20, bounce_time=0.01)
+
+
+t1.when_pressed = lambda: print('1 pressed!')
+# t1.when_released = lambda: print('1 released!')
+t2.when_pressed = lambda: print('==== 2 pressed! =====')
+t2.when_released = lambda: print('==== 2 released! =====')
+
+
+while True:
+    
+    
+    
+    pass
